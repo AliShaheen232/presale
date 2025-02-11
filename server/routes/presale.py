f371f247bb092ac6a154a519f5e0236448d08413
professionalali22 <54819672+AliShaheen232@users.noreply.github.com>
2025-02-12 02:01:33 +0500

    added .venv

---
+++ server/routes/presale.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Account(BaseModel):
+    address: str
+    chain:str
+    
+
+@app.get("/")
+async def root() :
+    return {"messgae":"presale APIs"}
+
+@app.get("/connect")
+async def root() :
